69da13bf3ed46bd8d3759ea20711d2fa85df5865
cristian stiven p <styven21121@gmail.com>
2021-05-03 21:57:15 -0500

    balance chart added

--- src/app/pages/finances/components/summary/summary.component.ts
+++ src/app/pages/finances/components/summary/summary.component.ts
@@ -23,60 +23,19 @@ export class SummaryComponent implements OnInit {
       {
         label: 'Acciones2',
         value: 200000,
-        color: 'red'
+        color: '#fc5c65'
       },
       {
         label: 'otra',
         value: 200001,
-        color: 'red'
+        color: '#fc5c65'
       }
     ],
     options: {
       hoverOffset: 0,
-      cutout: 110,
-      padding: 0
+      cutout: 85,
+      padding: 30
     }
-  };
-
-  bar2 = {
-    type: 'bar',
-    data: {
-      labels: ['Noviembre', 'diciembre', 'enero', 'febrero', 'marzo', 'abril'],
-      datasets: [
-        {
-          barThickness: 25,
-          label: 'My First Dataset',
-          data: [3000000, 300000, 2678000, 3000000, 3000000, 300000, 150000],
-          backgroundColor: [
-            'rgba(255, 99, 132 )',
-            'rgba(255, 159, 64 )',
-            'rgba(255, 205, 86 )',
-            'rgba(75, 192, 192 )',
-            'rgba(54, 162, 235 )',
-            'rgba(153, 102, 255)',
-            'rgba(201, 203, 207)'
-          ],
-          borderColor: 'transparent',
-          borderWidth: 1
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-      plugins: {
-        legend: {
-          display: false,
-        },
-      },
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      },
-      onClick: (e, arr, chart) => {
-        console.log([e, arr, chart]);
-      }
-    },
   };
 
   ngOnInit(): void {
