0920c862151078d9b0f006378c4a51df7de6a60a
cristian stiven p <styven21121@gmail.com>
2021-05-18 21:49:50 -0500

    budget pages

---
+++ src/app/core/components/progress/progress.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progress',
+  host: { class: 'row' },
+  template: `<div class="column" [style.width]="progress">{{progress}}</div>`,
+  styleUrls: ['./progress.component.scss']
+})
+export class ProgressComponent implements OnInit {
+  @Input()
+  set progress(value: string | number) {
+    this.progressValue = `${value}%`;
+  }
+  get progress(): string | number {
+    return this.progressValue;
+  }
+
+  private progressValue: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
