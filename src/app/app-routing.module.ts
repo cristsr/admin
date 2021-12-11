9105f343658489ca91e589e3088943218903403a
cristsr <styven21121@gmail.com>
2021-12-11 03:03:34 -0500

    feat: routing config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: 'finances',
+  },
+
+  // After the user signs in, the sign in page will redirect the user to the 'signed-in-redirect'
+  // path. Below is another redirection for that path to redirect the user to the desired
+  // location. This is a small convenience to keep all main routes together here on this file.
+  {
+    path: 'signed-in-redirect',
+    pathMatch : 'full',
+    redirectTo: 'finances'
+  },
+
+  {
+    path: '',
     component: LayoutComponent,
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    data: {
+      layout: 'default'
+    },
     children: [
-      {
-        path: 'education',
-        loadChildren: () => import('./modules/education/education.module').then(m => m.EducationModule),
-      },
       {
         path: 'finances',
         loadChildren: () => import('./modules/finances/finances.module').then(m => m.FinancesModule)
       },
       {
-        path: 'health',
-        loadChildren: () => import('./modules/health/health.module').then(m => m.HealthModule),
+        path: 'education',
+        loadChildren: () => import('./modules/education/education.module').then(m => m.EducationModule),
       },
       {
-        path: '',
-        redirectTo: 'finances'
+        path: 'health',
+        loadChildren: () => import('./modules/health/health.module').then(m => m.HealthModule),
       }
     ]
   },
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,6 +3,7 @@ import { DOCUMENT } from '@angular/common';
 import { SidebarComponent } from './common/sidebar/sidebar.component';
 import { LayoutService } from '../core/services/layout/layout.service';
 import { Menu, Submenu } from '../core/interfaces/menu';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -10,32 +11,39 @@ import { Menu, Submenu } from '../core/interfaces/menu';
     class: 'relative flex h-screen w-screen'
   },
   template: `
-    <!-- Sidebar -->
-    <app-sidebar
-      [menu]="menu"
-      (menuChange)="onLinkClick($event)">
-    </app-sidebar>
+    <ng-template [ngIf]="layout === 'empty'">
+      <router-outlet></router-outlet>
+    </ng-template>
 
-    <div class="flex flex-col h-screen w-full">
-      <!-- Navbar -->
-      <app-nav
-        (menuToggle)="toggleSidebar()"
-        [title]="navTitle">
-      </app-nav>
+    <ng-template [ngIf]="layout === 'default'">
+      <!-- Sidebar -->
+      <app-sidebar
+        [menu]="menu"
+        (menuChange)="onLinkClick($event)">
+      </app-sidebar>
 
-      <!-- Content -->
-      <div class="flex flex-col h-screen overflow-y-auto bg-gray-50">
-        <router-outlet></router-outlet>
+      <div class="flex flex-col h-screen w-full">
+        <!-- Navbar -->
+        <app-nav
+          (menuToggle)="toggleSidebar()"
+          [title]="navTitle">
+        </app-nav>
+
+        <!-- Content -->
+        <div class="flex flex-col h-screen overflow-y-auto bg-gray-50">
+          <router-outlet></router-outlet>
+        </div>
+
+        <!-- BottomNav -->
+        <app-bottom-nav
+          *ngIf="showBottomNav"
+          linkActiveClass="text-blue-500"
+          [config]="currentSubmenu"
+          (action)="onAction($event)">
+        </app-bottom-nav>
       </div>
+    </ng-template>
 
-      <!-- BottomNav -->
-      <app-bottom-nav
-        *ngIf="showBottomNav"
-        linkActiveClass="text-blue-500"
-        [config]="currentSubmenu"
-        (action)="onAction($event)">
-      </app-bottom-nav>
-    </div>
   `,
   styleUrls: ['./layout.component.scss']
 })
@@ -126,23 +134,30 @@ export class LayoutComponent implements OnInit {
 
   currentSubmenu: Submenu[];
 
+  layout: 'default' | 'empty';
+
   get navTitle(): string {
-    return this.layout.navTitle;
+    return this.layoutService.navTitle;
   }
 
   get showBottomNav(): boolean {
-    return this.layout.showBottomNav;
+    return this.layoutService.showBottomNav;
   }
 
   constructor(
     @Inject(DOCUMENT)
     private document: Document,
-    private layout: LayoutService
+    private layoutService: LayoutService,
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     const defaultMenu = this.menu.find(v => v.default);
     this.showOrHideBottomNav(defaultMenu);
+
+    this.activatedRoute.data.subscribe({
+      next: ({layout}) => this.layout = layout,
+    });
   }
 
   onAction(event: any): void {
@@ -157,10 +172,10 @@ export class LayoutComponent implements OnInit {
 
   showOrHideBottomNav(menu: Menu): void {
     if (menu.submenu) {
-      this.layout.showBottomNav = true;
+      this.layoutService.showBottomNav = true;
       this.currentSubmenu = menu.submenu;
     } else {
-      this.layout.showBottomNav = false;
+      this.layoutService.showBottomNav = false;
     }
   }
 
