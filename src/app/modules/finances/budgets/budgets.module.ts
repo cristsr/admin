2998130c655b34a54d50de6432c8fcc4fdfd2a26
cristsr <styven21121@gmail.com>
2022-02-16 14:15:21 -0500

    feat: order imports

--- src/app/modules/finances/budgets/budgets.module.ts
+++ src/app/modules/finances/budgets/budgets.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { BudgetsRoutingModule } from './budgets-routing.module';
 import { BudgetsComponent } from './budgets.component';
 import { ProgressModule } from 'core/components/progress/progress.module';
-import { ThousandSuffixesModule } from 'core/pipes/thousand-suffixes/thousand-suffixes.module';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { ThousandSuffixesModule } from 'core/pipes/thousand-suffixes';
 
 @NgModule({
   declarations: [BudgetsComponent],
