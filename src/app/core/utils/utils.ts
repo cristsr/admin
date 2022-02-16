b78d21b498ebc6655a8518e4de6f90d69f264207
cristsr <styven21121@gmail.com>
2022-02-16 16:54:45 -0500

    refactor: remove fn in favor to use cdk

--- src/app/core/utils/utils.ts
+++ src/app/core/utils/utils.ts
@@ -1,13 +1,3 @@
-export function convertToBool(val: any): boolean {
-  if (typeof val === 'string') {
-    val = val.toLowerCase().trim();
-
-    return val === 'true' || val === '';
-  }
-
-  return !!val;
-}
-
 export function isNotNullOrUndefined(val: any): boolean {
   return val !== null && val !== undefined;
 }
