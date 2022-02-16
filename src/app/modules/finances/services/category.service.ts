937066984d4fdf7784035391d41442e11bbb58a8
cristsr <styven21121@gmail.com>
2022-02-16 14:15:50 -0500

    feat: inject config service

---
+++ src/app/modules/finances/services/category.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ConfigService } from 'core/services';
+import { ENV } from 'environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+  constructor(private httpClient: HttpClient, private config: ConfigService) {}
+
+  get categories$(): Observable<any[]> {
+    const url = this.config.get(ENV.FINANCES_API) + '/categories';
+    return this.httpClient.get<any>(url);
+  }
+}
