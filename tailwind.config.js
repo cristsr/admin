aca572e9a843e6b1c6c6e3b2505e460838b43c7f
cristsr <styven21121@gmail.com>
2022-02-16 14:06:31 -0500

    fix: tailwind upgrade config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,24 @@
-const { guessProductionMode } = require("@ngneat/tailwind");
+const { guessProductionMode } = require('@ngneat/tailwind');
 
 process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
 
 module.exports = {
-    prefix: '',
-    mode: 'jit',
-    purge: {
-      content: [
-        './src/**/*.{html,ts,css,scss,sass,less,styl}',
-      ]
-    },
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {
-        gridTemplateColumns: {
-          'auto': 'auto 1fr',
-        },
-        gridTemplateRows: {
-          'auto': 'auto 1fr',
-        }
+  prefix: '',
+  mode: 'jit',
+  content: ['./src/**/*.{html,ts,css,scss,sass}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        auto: 'auto 1fr',
+      },
+      gridTemplateRows: {
+        auto: 'auto 1fr',
       },
     },
-    variants: {
-      extend: {
-      },
-    },
-    plugins: [
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/typography')
-    ],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
