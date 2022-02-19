82c3d44f2b579cf35c04bfe36cbb0cc87058dedd
cristsr <styven21121@gmail.com>
2022-02-18 23:38:45 -0500

    feat: infinite scroll init files

--- src/app/core/directives/infinite-scroll/infinite-scroll.directive.spec.ts
+++ src/app/core/directives/infinite-scroll/infinite-scroll.directive.spec.ts
@@ -2,7 +2,7 @@ import { InfiniteScrollDirective } from './infinite-scroll.directive';
 
 describe('InfiniteScrollDirective', () => {
   it('should create an instance', () => {
-    const directive = new InfiniteScrollDirective();
+    const directive = new InfiniteScrollDirective({} as any);
     expect(directive).toBeTruthy();
   });
 });
