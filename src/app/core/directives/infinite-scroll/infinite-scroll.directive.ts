5c03fb16ba2909a3d899b37f82bcb9672820134f
cristsr <styven21121@gmail.com>
2022-02-19 00:28:59 -0500

    feat: stable infinite scroll directive

--- src/app/core/directives/infinite-scroll/infinite-scroll.directive.ts
+++ src/app/core/directives/infinite-scroll/infinite-scroll.directive.ts
@@ -1,15 +1,77 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  EventEmitter,
+  Input,
+  NgZone,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
 
 @Directive({
   selector: '[appInfiniteScroll]',
 })
 export class InfiniteScrollDirective implements OnInit {
-  @Input() target: ElementRef;
+  @Input() target: HTMLElement;
 
-  constructor(private root: ElementRef) {}
+  @Input() threshold = 1;
+
+  @Input() disabled = false;
+
+  @Input()
+  get completed(): boolean {
+    return this._completed;
+  }
+  set completed(val: BooleanInput) {
+    this._completed = coerceBooleanProperty(val);
+    if (this._completed) {
+      this.stopObserving();
+    }
+  }
+  private _completed = false;
+
+  @Output() scrolled = new EventEmitter<IntersectionObserverEntry>();
+
+  private intersectionObserver: IntersectionObserver;
+
+  constructor(private ngZone: NgZone, private root: ElementRef) {}
 
   ngOnInit(): void {
     console.log('Root: ', this.root);
     console.log('Target: ', this.target);
+
+    this.configure();
+  }
+
+  configure(): void {
+    this.ngZone.runOutsideAngular(() => {
+      const config = {
+        root: this.root.nativeElement,
+        threshold: this.threshold,
+      };
+
+      const callback = (entries: IntersectionObserverEntry[]) => {
+        entries.forEach((entry: IntersectionObserverEntry) => {
+          if (entry.isIntersecting) {
+            if (this.disabled) {
+              return;
+            }
+
+            this.scrolled.emit(entry);
+          }
+        });
+      };
+
+      this.intersectionObserver = new IntersectionObserver(callback, config);
+
+      this.intersectionObserver.observe(this.target);
+    });
+  }
+
+  stopObserving(): void {
+    this.ngZone.runOutsideAngular(() => {
+      this.intersectionObserver.disconnect();
+    });
   }
 }
