6ec5ce11c79b0067b1460fe063c3280238bf2f81
cristsr <styven21121@gmail.com>
2022-03-04 20:13:08 -0500

    chore: remove unused code

--- src/app/modules/finances/pages/movements/movements.module.ts
+++ src/app/modules/finances/pages/movements/movements.module.ts
@@ -6,20 +6,13 @@ import { MovementsComponent } from './movements.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRippleModule } from '@angular/material/core';
-import { MovementDetailComponent } from './detail/movement-detail.component';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MovementsListComponent } from './list/movements-list.component';
 import { InfiniteScrollModule } from 'core/directives/infinite-scroll';
-import { GroupByModule } from 'modules/finances/pipes/group-by';
 import { DatetimeModule } from 'core/pipes/datetime';
 import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
-  declarations: [
-    MovementsComponent,
-    MovementsListComponent,
-    MovementDetailComponent,
-  ],
+  declarations: [MovementsComponent],
   imports: [
     CommonModule,
     MovementsRoutingModule,
@@ -27,10 +20,9 @@ import { MatMenuModule } from '@angular/material/menu';
     MatIconModule,
     MatRippleModule,
     MatDialogModule,
-    InfiniteScrollModule,
-    GroupByModule,
-    DatetimeModule,
     MatMenuModule,
+    DatetimeModule,
+    InfiniteScrollModule,
   ],
 })
 export class MovementsModule {}
