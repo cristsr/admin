1d9dcf505a02f0326ce35787b84cbe944fefd128
cristsr <styven21121@gmail.com>
2022-03-05 22:34:22 -0500

    fix: unsubscribe all on destroy

--- src/app/modules/finances/pages/movements/movements.component.ts
+++ src/app/modules/finances/pages/movements/movements.component.ts
@@ -2,6 +2,7 @@ import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
+  OnDestroy,
   OnInit,
 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
@@ -16,18 +17,20 @@ import { MovementService } from 'modules/finances/services';
 import { Pageable } from 'core/types';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { MovementFormComponent } from 'modules/finances/pages/movement-form/movement-form.component';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-movements',
   templateUrl: 'movements.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MovementsComponent implements OnInit {
+export class MovementsComponent implements OnInit, OnDestroy {
   activeScroll: boolean;
   completedScroll: boolean;
   movements: GroupMovement[];
-
   queryParams: MovementQuery;
+
+  private unsubscribeAll = new Subject<void>();
 
   set groupBy(value: GroupBy) {
     if (this.queryParams.groupBy === value) {
@@ -47,22 +50,29 @@ export class MovementsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.movementService.movements$.subscribe({
-      next: (response: Pageable<GroupMovement>) => {
-        this.movements.push(...response.data);
-        this.activeScroll = true;
+    this.movementService.movements$
+      .pipe(takeUntil(this.unsubscribeAll))
+      .subscribe({
+        next: (response: Pageable<GroupMovement>) => {
+          this.movements.push(...response.data);
+          this.activeScroll = true;
 
-        if (response.lastPage) {
-          this.completedScroll = true;
-        }
+          if (response.lastPage) {
+            this.completedScroll = true;
+          }
 
-        this.changeDetectorRef.detectChanges();
-        console.log(this.movements);
-      },
-    });
+          this.changeDetectorRef.detectChanges();
+          console.log(this.movements);
+        },
+      });
 
     this.setInitialConfig();
     this.movementService.nextPage(this.queryParams);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeAll.next();
+    this.unsubscribeAll.complete();
   }
 
   setInitialConfig(groupBy: GroupBy = 'days'): void {
