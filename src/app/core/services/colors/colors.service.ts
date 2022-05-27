3fccf5358a02da4d0f149bf8ec05c2dab31554c9
cristsr <styven21121@gmail.com>
2022-05-26 23:58:09 -0500

    fix: add color service

---
+++ src/app/core/services/colors/colors.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import * as Colors from 'tailwindcss/colors';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ColorsService {
+  constructor() {}
+
+  classToHex(cssClass: string): string {
+    const split = cssClass.split('-');
+
+    if (split.length === 2) {
+      const [color, code] = split;
+      return Colors[color][code];
+    }
+
+    if (split.length === 3) {
+      const [_, color, code] = split;
+      return Colors[color][code];
+    }
+
+    console.warn(`Could not convert ${cssClass} to hex`);
+
+    return '';
+  }
+}
