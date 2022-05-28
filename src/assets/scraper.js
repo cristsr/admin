a1a1d72609928ddd77da4168fb84058e6c90b5b9
cristsr <styven21121@gmail.com>
2022-05-27 22:16:43 -0500

    feat: format code

--- src/assets/scraper.js
+++ src/assets/scraper.js
@@ -4,7 +4,7 @@ async function main() {
   const length = document.querySelectorAll('.categoryButton').length;
 
   for (let i = 0; i < length; i++) {
-    await waitSeconds(1)
+    await waitSeconds(1);
 
     const buttons = document.querySelectorAll('.categoryButton');
 
@@ -16,11 +16,11 @@ async function main() {
 
     category.click();
 
-    await waitSeconds(1)
+    await waitSeconds(1);
 
     const subcats = Array.from(document.querySelectorAll('.subCategoryElement'))
-      .map(subcat => subcat.innerText.toLowerCase())
-      .map(subcat => ({name: subcat}));
+      .map((subcat) => subcat.innerText.toLowerCase())
+      .map((subcat) => ({ name: subcat }));
 
     console.log(subcats);
 
@@ -28,20 +28,18 @@ async function main() {
       icon: '',
       color: '',
       name: categoryName.toLowerCase(),
-      subcategories: subcats
+      subcategories: subcats,
     });
 
-    const back = document.querySelector('.finerio-accent-outline-color')
+    const back = document.querySelector('.finerio-accent-outline-color');
     back.click();
-
   }
 
   return categories;
-
 }
 
 function waitSeconds(seconds) {
-  return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 }
 
-main().then(result => console.log(JSON.stringify(result)));
+main().then((result) => console.log(JSON.stringify(result)));
