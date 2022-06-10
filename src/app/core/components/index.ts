26ec714a5a3ac65d6693bdf36770933d913255e7
cristsr <styven21121@gmail.com>
2022-06-09 23:05:02 -0500

    feat: loader

--- src/app/core/components/index.ts
+++ src/app/core/components/index.ts
@@ -1,3 +1,4 @@
 export * from './list-item';
 export * from './progress';
 export * from './select';
+export * from './loader';
---
+++ src/app/core/components/loader/loader.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { EventEmitterService } from 'core/services';
+import { distinctUntilChanged } from 'rxjs';
+
+@Component({
+  selector: 'app-loader',
+  template: `
+    <div
+      *ngIf="show"
+      class="absolute top-0 left-0 right-0 bottom-0 bg-gray-500 z-[9999] opacity-80 flex items-center justify-center"
+    >
+      <span class="loader"></span>
+    </div>
+  `,
+  styleUrls: ['./loader.component.scss'],
+})
+export class LoaderComponent implements OnInit {
+  show = false;
+
+  constructor(private emitter: EventEmitterService) {}
+
+  ngOnInit(): void {
+    this.emitter
+      .on('loader:show')
+      .pipe(distinctUntilChanged())
+      .subscribe((show: boolean) => {
+        console.log('[LoaderComponent] Update loader state', show);
+        this.show = show;
+      });
+  }
+}
