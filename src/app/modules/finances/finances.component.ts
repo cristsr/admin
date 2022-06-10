9a658453cd32922330d1f9e83a2198ba3a4b7e87
cristsr <styven21121@gmail.com>
2022-06-09 23:12:35 -0500

    perf: remove unused code

--- src/app/modules/finances/finances.component.ts
+++ src/app/modules/finances/finances.component.ts
@@ -3,9 +3,7 @@ import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { filter, Subject, takeUntil } from 'rxjs';
 import { EventEmitterService } from 'core/services';
 import { Events } from 'layout/constants';
-import { Submenu } from 'layout/types';
 import { MovementFormComponent } from 'modules/finances/components';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-finances',
@@ -15,32 +13,16 @@ export class FinancesComponent implements OnInit, OnDestroy {
   #unsubscribeAll = new Subject<void>();
 
   constructor(
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
     private emitter: EventEmitterService,
     private bottomSheet: MatBottomSheet,
   ) {}
 
   ngOnInit(): void {
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe({
-        next: (event) => {
-          console.log('[FinancesComponent] router.events', event);
-        },
-      });
-
-    this.activatedRoute.data.subscribe({
-      next: (url) => {
-        console.log('[FinancesComponent] url', url);
-      },
-    });
-
     this.emitter
       .on(Events.BOTTOM_NAV_ACTION)
       .pipe(
         takeUntil(this.#unsubscribeAll),
-        filter((action: Submenu) => action.tag === 'add-movement'),
+        filter((tag) => tag === 'add-movement'),
       )
       .subscribe({
         next: () => {
