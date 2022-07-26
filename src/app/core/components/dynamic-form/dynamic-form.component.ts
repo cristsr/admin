f7c8bcef3a47f4da8d782a7d0f8f69611937b04c
cristsr <styven21121@gmail.com>
2022-07-25 22:26:59 -0500

    feat: dynamic form component

---
+++ src/app/core/components/dynamic-form/dynamic-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dynamic-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dynamic-form.component.html',
+  styleUrls: ['./dynamic-form.component.scss'],
+})
+export class DynamicFormComponent {}
